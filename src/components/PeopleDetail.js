7b36a4be0d7d52979c8f4850cc018da6c48ddbdd
padrisimo <chamizho@gmail.com>
2018-07-21 22:14:05 +0200

    coding scroll on detail view

--- src/components/PeopleDetail.js
+++ src/components/PeopleDetail.js
@@ -10,9 +10,46 @@ import * as actions from '../actions';
 class PeopleDeatil extends Component {
   render() {
     return (
-      <View>
-        <Text> textInComponent </Text>
-      </View>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Image
+          source={require('../images/background.jpg')}
+          style={[theme.cardImageStyle, styles.image]}
+        />
+        <EvilIcon name={'user'} size={100} styles={styles.icon} />
+        <SimpleIcon name={'user'} size={30} styles={styles.closeicon}
+          onPress={() => this.props.noneSelected()}
+        />
+        <Text style={[theme.cardTitleStyle, style.title1]}>
+          {this.props.person.first_name} {this.props.person.last_name}
+        </Text>
+        <Text style={[theme.cardTitleStyle, style.title2]}>
+          from {this.props.person.company}
+        </Text>
+        <View style={styles.textArea}>
+          <MaterialIcon name={'phone'} size={40} style={styles.textIcons} />
+          <Text style={theme.cardContentStyle}>
+            {this.props.person.phone}
+          </Text>
+        </View>
+        <View style={styles.textArea}>
+          <MaterialIcon name={'email'} size={40} style={styles.textIcons} />
+          <Text style={theme.cardContentStyle}>
+            {this.props.person.email}
+          </Text>
+        </View>
+        <View style={styles.textArea}>
+          <MaterialIcon name={'assignment'} size={40} style={styles.textIcons} />
+          <Text style={theme.cardContentStyle}>
+            {this.props.person.project}
+          </Text>
+        </View>
+        <View style={styles.textArea}>
+          <MaterialIcon name={'mode-edit'} size={40} style={styles.textIcons} />
+          <Text style={theme.cardContentStyle}>
+            {this.props.person.notes}
+          </Text>
+        </View>
+      </ScrollView>
     )
   }
 }
