629f30445d95722049473654aa81ef146a64744f
padrisimo <chamizho@gmail.com>
2018-07-22 04:45:41 +0200

    add clickable buttons detail view

--- src/components/PeopleDetail.js
+++ src/components/PeopleDetail.js
@@ -8,6 +8,9 @@ import * as actions from '../actions';
 
 
 class PeopleDeatil extends Component {
+  handleClick = (link) => Linking.canOpenURL(link).then(supported => 
+    supported ? Linking.openURL(link) : console.log(`Can't open uri: ${link}`)
+  )
   render() {
     return (
       <ScrollView showsVerticalScrollIndicator={false}>
@@ -48,6 +51,28 @@ class PeopleDeatil extends Component {
           <Text style={theme.cardContentStyle}>
             {this.props.person.notes}
           </Text>
+        </View>
+        <View style={styles.actionArea}>
+          <TouchableOpacity 
+            onPress={() => this.handleClick(`tel:${this.props.person.phone}`)}          
+          >
+            <Image src={require('../images/call@2x.png')} style={styles.actionImage}/>
+          </TouchableOpacity>
+          <TouchableOpacity 
+            onPress={() => this.handleClick(`sms:${this.props.person.phone}`)}          
+          >
+            <Image src={require('../images/sms@2x.png')} style={styles.actionImage}/>
+          </TouchableOpacity>
+          <TouchableOpacity 
+            onPress={() => this.handleClick(`mailto:${this.props.person.email}`)}          
+          >
+            <Image src={require('../images/email@2x.png')} style={styles.actionImage}/>
+          </TouchableOpacity>
+          <View style={styles.actionArea}>
+            <Text>Call</Text>
+            <Text>SMS</Text>
+            <Text>Emaill</Text>
+          </View>
         </View>
       </ScrollView>
     )
